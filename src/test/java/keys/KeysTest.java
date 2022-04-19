ece7e826a19e13e4c4eaaee2f5d04f22c797d28e
trollkuro <kristi.egorova@gmail.com>
2022-04-19 11:49:27 +0300

    lessons 1 - 7.1

---
+++ src/test/java/keys/KeysTest.java
@@ -0,0 +1,17 @@
+package keys;
+
+import base.BaseTests;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class KeysTest extends BaseTests {
+
+    @Test
+    public void testBackSpace(){
+        var keyPage = homePage.clickKeyPresses();
+        keyPage.enterText("A" + Keys.BACK_SPACE);
+        assertEquals(keyPage.getResult(), "You entered: BACK_SPACE");
+    }
+}
